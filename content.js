4f6bdd0efdac7bc42f97fbedd7fa1cc50b424c7b
gosienna <gosienna@gmail.com>
2025-08-22 23:41:20 +0800

    add hoving effect for element selection

--- content.js
+++ content.js
@@ -1,16 +1,18 @@
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === "crawl") {
-    try {
-      const results = document.evaluate(request.xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
-      const data = [];
-      for (let i = 0; i < results.snapshotLength; i++) {
-        const node = results.snapshotItem(i);
-        data.push(node.textContent.trim());
-      }
-      sendResponse({ data: data });
-    } catch (e) {
-      sendResponse({ error: e.message });
+    if (request.action === "crawl") {
+        try {
+            const results = document.evaluate(request.xpath, document, null, XPathResult.ORDERED_NODE_SNAPSHOT_TYPE, null);
+            const data = [];
+            for (let i = 0; i < results.snapshotLength; i++) {
+                const node = results.snapshotItem(i);
+                data.push(node.textContent.trim());
+            }
+            sendResponse({ data: data });
+        } catch (e) {
+            sendResponse({ error: e.message });
+        }
+    } else if (request.action === "toggleInspector") {
+        window.xpathCrawler.toggleInspector();
     }
-  }
-  return true; // Indicates that the response is sent asynchronously
+    return true; // Indicates that the response is sent asynchronously
 });
--- popup.js
+++ popup.js
@@ -1,7 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
   const crawlButton = document.getElementById('crawlButton');
+  const selectElementButton = document.getElementById('selectElementButton');
   const xpathInput = document.getElementById('xpathInput');
   const resultsTextarea = document.getElementById('results');
+
+  // Listener for receiving messages from the content script
+  chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+    if (request.action === "elementSelected") {
+      xpathInput.value = request.xpath;
+    }
+  });
+
+  selectElementButton.addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: "toggleInspector" });
+    });
+  });
 
   crawlButton.addEventListener('click', () => {
     const xpath = xpathInput.value;
